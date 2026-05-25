dc39162c2735a6f8cc122eabc63f74430cd0b5fe
SuspiciousApplePie <andreiusita6@gmail.com>
2026-05-25 16:57:05 +0800

    Add Node() test suite

--- js/knightsTravails.test.js
+++ js/knightsTravails.test.js
@@ -1,3 +1,10 @@
+
+describe("Node test", () => {
+  test("Default node", () => {
+    expect(Node()).toStrictEqual({ value: null, nextNode: null });
+  });
+});
+
 describe("Array Comparison", () => {
   test("Not equal array", () => {
     expect(isArrayEqual([2, 1], [1, 2])).toBe(false);
