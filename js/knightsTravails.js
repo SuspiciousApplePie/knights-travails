829bf4417022ba6460869408c29eea33517f292e
SuspiciousApplePie <andreiusita6@gmail.com>
2026-05-25 22:17:34 +0800

    Add knightMoves(start, end) this will locate the fastest path to end

--- js/knightsTravails.js
+++ js/knightsTravails.js
@@ -1,8 +1,102 @@
+export function knightMoves(start, end) {
+  const paths = Queue();
+  paths.enqueue([start]);
+  const visited = new Set();
+
+  while (true) {
+    let path = paths.dequeue();
+    let coordinate = path.at(-1);
+    let items = [
+      NNE(coordinate),
+      ENE(coordinate),
+      NNW(coordinate),
+      WNW(coordinate),
+      SSW(coordinate),
+      WSW(coordinate),
+      ESE(coordinate),
+      SSE(coordinate),
+    ];
+
+    let match = null;
+
+    items.forEach((item) => {
+      let pathCopy = path.slice();
+      if (item && !visited.has(item.join(","))) {
+        if (isArrayEqual(item, end) && isValidCoordinate(item)) {
+          pathCopy.push(item);
+          match = pathCopy;
+        } else if (isValidCoordinate(item)) {
+          pathCopy.push(item);
+          paths.enqueue(pathCopy);
+          visited.add(item.join(","));
+        }
+      }
+    });
+    console.log(match);
+    if (match) return match;
+  }
+
+  function NNE(coordinates) {
+    coordinates = [coordinates.at(0) + 2, coordinates.at(-1) + 1];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+
+  function ENE(coordinates) {
+    coordinates = [coordinates.at(0) + 1, coordinates.at(-1) + 2];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+
+  function NNW(coordinates) {
+    coordinates = [coordinates.at(0) + 2, coordinates.at(-1) - 1];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+
+  function WNW(coordinates) {
+    coordinates = [coordinates.at(0) + 1, coordinates.at(-1) - 2];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+  function SSW(coordinates) {
+    coordinates = [coordinates.at(0) - 2, coordinates.at(-1) - 1];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+
+  function WSW(coordinates) {
+    coordinates = [coordinates.at(0) - 1, coordinates.at(-1) - 2];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+
+  function ESE(coordinates) {
+    coordinates = [coordinates.at(0) - 1, coordinates.at(-1) + 2];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+
+  function SSE(coordinates) {
+    coordinates = [coordinates.at(0) - 2, coordinates.at(-1) + 1];
+    if (isValidCoordinate(coordinates)) return coordinates;
+    return;
+  }
+
+  function isValidCoordinate(coordinates) {
+    let isValid = true;
+    coordinates.forEach((coordinate) => {
+      if (coordinate < 0 || coordinate > 7) {
+        isValid = false;
+      }
+    });
+    return isValid;
+  }
+}
 
 export function Queue() {
   let head = null;
   let tail = null;
-
   return {
     enqueue: (value) => {
       if (!head) {
@@ -15,11 +109,10 @@ export function Queue() {
     },
     dequeue: () => {
       if (head) {
-        const dequeuedValue = head.value;
+        const dequeued = head;
         head = head.nextNode;
         if (!head) tail = head;
-
-        return dequeuedValue.value;
+        return dequeued.value;
       }
 
       return null;
--- js/knightsTravails.test.js
+++ js/knightsTravails.test.js
@@ -7,6 +7,54 @@ describe("Knight Moves", () => {
       [1, 2],
     ]);
   });
+
+  test("To center", () => {
+    expect(knightMoves([0, 0], [3, 3])).toStrictEqual([
+      [0, 0],
+      [2, 1],
+      [3, 3],
+    ]);
+  });
+
+  test("To base", () => {
+    expect(knightMoves([3, 3], [0, 0])).toStrictEqual([
+      [3, 3],
+      [1, 2],
+      [0, 0],
+    ]);
+  });
+
+  test("0,0 to 6,2", () => {
+    expect(knightMoves([0, 0], [6, 2])).toStrictEqual([
+      [0, 0],
+      [2, 1],
+      [4, 2],
+      [5, 4],
+      [6, 2],
+    ]);
+  });
+
+  test("0,0 to 7,0", () => {
+    expect(knightMoves([0, 0], [7, 0])).toStrictEqual([
+      [0, 0],
+      [2, 1],
+      [4, 2],
+      [6, 3],
+      [5, 1],
+      [7, 0],
+    ]);
+  });
+
+  test("0,0 to 7,7", () => {
+    expect(knightMoves([0, 0], [7, 7])).toStrictEqual([
+      [0, 0],
+      [2, 1],
+      [4, 2],
+      [6, 3],
+      [5, 1],
+      [7, 0],
+    ]);
+  });
 });
 
 describe("Node test", () => {
@@ -26,12 +74,9 @@ describe("Array Comparison", () => {
 
 describe("Queue", () => {
   test("Enqueue and Dequeue", () => {
-    const node = Node([1, 2]);
-    const node2 = Node([0, 0]);
     const queue = Queue();
-    queue.enqueue(node);
-    queue.enqueue(node2);
-    expect(queue.dequeue()).toStrictEqual([1, 2]);
-    expect(queue.dequeue()).toStrictEqual([0, 0]);
+    queue.enqueue([[0, 0]]);
+    queue.enqueue([[1, 2]]);
+    expect(queue.dequeue()).toStrictEqual([[0, 0]]);
   });
 });
